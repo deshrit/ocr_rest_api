48796eb6de74dcde79b0f6392437f63b3312e42b
deshrit baral <deshritbaral@gmail.com>
2023-07-04 19:35:31 +0545

    removed unnecessary cors from blueprints

--- app/ocr.py
+++ app/ocr.py
@@ -1,11 +1,8 @@
 from flask import Blueprint, request, jsonify
-# from flask_cors import CORS
 from . import utils
 
 bp = Blueprint("ocr", __name__, url_prefix="/api/v1/ocr")
-# CORS(bp)
 
-# @bp.route("/", methods=["POST"])
 @bp.route("/eng", methods=["POST"])
 def ocr():
     if not request.files:
